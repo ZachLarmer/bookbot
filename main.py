6ffda9a88663d8229c2333d07b52bc74c371ac0a
zlarm <zachary.larmer93@gmail.com>
2025-09-14 12:21:44 -0700

    read function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+## Bookbot functions
+
+## book text to string
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return(file_contents)
+
+
+## main calls the read function and prints
+def main():
+    print(get_book_text())
+
+
+## Call main
+main()
